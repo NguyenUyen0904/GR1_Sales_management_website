374824122fcab2fd6858f76fe11023f99db155d5
NguyenUyen0904 <nguyen.uyen.0904@gmail.com>
2021-07-04 22:22:38 +0700

    lay du lieu tu database

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -1,12 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
-    public function index(Request $request,$model){
-        return view('admin.listing');
+    public function index(Request $request,$modelName){
+        $adminUser=Auth::guard('admin')->user();
+        $model='\App\Models\\' . ucfirst($modelName);
+        $records=$model::paginate(1);
+        return view('admin.listing',[
+            'user'=>$adminUser,
+            'records'=>$records
+            ]
+        );
+
     }
 }
